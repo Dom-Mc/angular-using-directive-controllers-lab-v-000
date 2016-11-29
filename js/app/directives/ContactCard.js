594757773690eebb17def4576849caf411cd6260
Dom McKellar <dom.mckellar@gmail.com>
2016-11-29 14:38:26 -0800

    Done.

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -1,22 +1,28 @@
 function ContactCard() {
 	return {
-		scope: {
+		scope: {},
+		controllerAs: 'ctrl',
+		bindToController: {
 			name: '=',
-			email: '=',
 			phone: '=',
+			email: '=',
 			username: '='
 		},
+		restrict: "E",
+    controller: function(){
+      this.username = this.username.toLowerCase();
+    },
 		template: [
 			'<div>',
 				'<h4>Contact Card</h4>',
 				'<label>Name:</label>',
-				'{{ name }}',
+				'{{ ctrl.name }}',
 				'<label>Email:</label>',
-				'{{ email }}',
+				'{{ ctrl.email }}',
 				'<label>Phone:</label>',
-				'{{ phone }}',
+				'{{ ctrl.phone }}',
 				'<label>Username:</label>',
-				'<span class="username">{{ username }}</span>',
+				'<span class="username">{{ ctrl.username }}</span>',
 			'</div>'
 		].join(''),
 		restrict: 'E'
